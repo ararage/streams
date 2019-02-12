f3455bf107bf06c308b45af509759f1735e72790
josericardo.perez <josericardo.perez@bbva.com>
2019-02-11 19:45:49 -0600

    Added StreamForm component that can be used as base component for the Edition and Creation component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
  
 class StreamEdit extends React.Component {
+
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
     render (){
+        if(!this.props.stream){
+            return <div>Loading...</div>
+        }
         return(
             <div>
-                Stream Edit
+                { this.props.stream.title }
             </div>
         );
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    console.log(ownProps);
     return { 
-        stream: null
+        stream: state.streams[ownProps.match.params.id]
     };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
